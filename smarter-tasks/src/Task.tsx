d5b1334a47bc1f7df1561dc31c0874df26c16bda
unknown <satvikreddy2017@gmail.com>
2023-04-03 23:58:45 +0530

    L3 fixed

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './TaskCard.css'
 
 interface TaskProp {
   title: string
@@ -9,9 +10,9 @@ interface TaskProp {
 class Task extends React.Component<TaskProp> {
   render() {
     return (
-      <div className="TaskItem shadow-md border border-slate-100 my-4">
+      <div className="TaskItem">
         <h2 className="text-base font-bold my-1">{this.props.title}</h2>
-        <p className="text-sm">{this.props.dueDate}</p>
+        <p className="text-sm">Due Date: {this.props.dueDate}</p>
         <p className="text-sm">
           Description: {this.props.description}
         </p>
--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -27,9 +27,9 @@ class TaskForm extends React.Component<TaskFormProps, TaskFormState> {
       dueDate: this.state.dueDate,
       description: this.state.description
     }
-    if(newTask.title.trim().length === 0 || newTask.dueDate.length === 0) {
-      return
-    }
+    // if(newTask.title.trim().length === 0 || newTask.dueDate.length === 0) {
+    //   return
+    // }
     this.props.addTask(newTask);
     this.setState({ title: "", description:"", dueDate:"" });
   };
@@ -38,11 +38,11 @@ class TaskForm extends React.Component<TaskFormProps, TaskFormState> {
     return (
       <form onSubmit={this.addTask}>
         <label htmlFor="title" className="text-left">Title</label>
-        <input type="text" name="title" className="border-2 rounded w-full text-base p-2 my-2 border-blue-200" value={this.state.title} onChange={this.titleChanged}/>
+        <input type="text" name="title" required className="border-2 rounded w-full text-base p-2 my-2 border-blue-200" value={this.state.title} onChange={this.titleChanged}/>
         <label htmlFor="todoDescription">Description</label>
         <textarea id="todoDescription" cols={30} rows={3} value={this.state.description} onChange={this.descriptionChanged} className="border-2 w-full my-3 border-blue-200" />
         <label htmlFor="todoDueDate">Due Date</label>
-        <input type="date" id="todoDueDate" value={this.state.dueDate} onChange={this.dateChanged} className="border-2 w-full my-3 text-base p-2 border-blue-200"/>
+        <input type="date" id="todoDueDate" required value={this.state.dueDate} onChange={this.dateChanged} className="border-2 w-full my-3 text-base p-2 border-blue-200"/>
         <button type="submit" id="addTaskButton" className="w-full text-white bg-blue-600 hover:bg-blue-800 rounded py-2 px-2 font-bold">Add Items</button>
       </form>
     )
