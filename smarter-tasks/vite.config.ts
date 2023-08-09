bf081222d3c102ef9e59097de1bf38ecaec2d3db
Sathvik <satvikreddy2017@gmail.com>
2023-08-09 16:43:16 +0530

    L7 Fixed vite config

--- smarter-tasks/vite.config.ts
+++ smarter-tasks/vite.config.ts
@@ -2,7 +2,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default ({ mode }) => {
+export default ({ mode }: any) => {
   return defineConfig({
     build: {
       outDir: 'build',
