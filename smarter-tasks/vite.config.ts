1628d6832ff3b3cbb1f8226b91c4bd0a20fdeec7
Sathvik <satvikreddy2017@gmail.com>
2023-08-23 23:29:56 +0530

    L9 PWA, lazy loading

--- smarter-tasks/vite.config.ts
+++ smarter-tasks/vite.config.ts
@@ -1,13 +1,38 @@
-/* eslint-disable import/no-anonymous-default-export */
-import { defineConfig } from "vite";
+import { PluginOption, defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-export default ({ mode }: any) => {
+export default ({ mode } : { mode: any}) => {
   return defineConfig({
     build: {
       outDir: "build",
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        devOptions: {
+          enabled: true, // For making sure that the PWA is testable from the Local dev environment
+        },
+        manifest: {
+          name: "Smarter Tasks application",
+          short_name: "Smarter Tasks",
+          icons: [
+            {
+              src: "/pwa-192x192.png",
+              type: "image/png",
+              sizes: "192x192",
+            },
+            {
+              src: "/pwa-512x512.png",
+              type: "image/png",
+              sizes: "512x512",
+              purpose: "any maskable", // Icon format that ensures that your PWA icon looks great on all Android devices
+            },
+          ],
+          theme_color: "#AAF",
+        },
+      }),
+    ],
     define: {
       "process.env.NODE_ENV": `"${mode}"`,
     },
