d5d02bf6a6fc0476844c306a0fad8f644ec1dbe8
Sathvik <satvikreddy2017@gmail.com>
2023-08-23 23:57:51 +0530

    L9 Added lazy loading to others

--- smarter-tasks/src/pages/project_details/index.tsx
+++ smarter-tasks/src/pages/project_details/index.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 import ProjectDetails from "./ProjectDetails";
 
 import { Outlet } from "react-router-dom";
 import { TasksProvider } from "../../context/task/context";
+import ErrorBoundary from "../../components/ErrorBoundary";
 
 const ProjectDetailsIndex: React.FC = () => {
   return (
-    <TasksProvider>
-      <ProjectDetails />
-      <Outlet />
-    </TasksProvider>
+    <ErrorBoundary>
+        <Suspense fallback={<div className="suspense-loading">Loading...</div>}>
+          <TasksProvider>
+            <ProjectDetails />
+            <Outlet />
+          </TasksProvider>
+        </Suspense>
+      </ErrorBoundary>
   );
 };
 
